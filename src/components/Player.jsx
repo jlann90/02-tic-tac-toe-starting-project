265eaaaff3a382dd6594be8fa934a688c352e142
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-24 14:53:23 -0700

    feat: update Player function so player names can now be edited and saved

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
+  // Toggles editing state, "editing" is a prop passed into the setIsEditing state function housed within the handleEditClick function
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  // Handles the change in the input field, updates the playerName state with the value from the input field, the event is created by the onChange event listener and creates an object that contains the value of the input field
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = "Save";
   }
 
   return (
     <li>
-      <span className="player-info">
-        {playerName}
+      <span className="player">
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }

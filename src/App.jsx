ee6b82df59ca05b155542d30e38ecd9c3bcee321
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-29 12:29:59 -0700

    refactor: remove state tracking the active player, instead derive state from the gameTurns

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,34 @@ import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurnsArg) {
+  let currentPlayer = "X";
+
+  // Determine which player should be assigned to this move
+  if (gameTurnsArg.length > 0 && gameTurnsArg[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
-  // State for tracking which player's turn it is ("X" or "O")
-  const [activePlayer, setActivePlayer] = useState("X");
   // State for tracking all moves made in the game
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   // Handles when a square is selected on the board
   function handleSelectSquare(rowIndex, colIndex) {
-    // Switch to the next player after a move
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     // Add the new move to the list of turns
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      // Determine which player should be assigned to this move
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       // Create a new array of turns with the latest move at the front
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
       ];
-
       return updatedTurns;
     });
   }

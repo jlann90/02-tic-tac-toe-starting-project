73666f4d140c9ece3561444ce420ebdac61d6004
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-28 17:36:04 -0700

    feat: update Log output a list entry tracking the player and their selection

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ function App() {
           />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
-        <Log />
       </div>
+      <Log turns={gameTurns} />
     </main>
   );
 }
